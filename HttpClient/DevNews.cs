4d897d99e4a9fa3098ed08955d492df2e579be75
YeruhamMendelson <y0583207549@gmail.com>
2025-05-26 15:44:36 +0300

    exercises 1 - get 5 posts

--- HttpClient/DevNews.cs
+++ HttpClient/DevNews.cs
@@ -1,9 +1,37 @@
 ﻿using System.Net.Http;
-public class Client
+using System.Threading.Tasks;
+using System.Text.Json;
+using System;
+
+
+public  class Client
 {
+    string url;
+    HttpClient client = new HttpClient();
     public Client(string url)
     {
-        //HttpClient client = new HttpClient();
-
+        this.url = url;
     }
+
+    public class Post
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string bady { get; set; }
+    }
+    public async Task fetchPosts(int numPosts)
+    {
+
+        var response = await client.GetAsync(this.url);
+        string answer = await response.Content.ReadAsStringAsync();
+        Post[] posts = JsonSerializer.Deserialize<Post[]>(answer);
+
+        for (int i = 0; i < numPosts; i++)
+        {
+            Post post = posts[i];
+            Console.WriteLine(post.title);
+        }
+    }
+
 }
