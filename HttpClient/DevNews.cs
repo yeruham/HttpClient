f21108acc70e9d4d4c78756433f480b7828d1cf2
YeruhamMendelson <y0583207549@gmail.com>
2025-05-26 16:07:56 +0300

    exercieses 2 - get post by id

--- HttpClient/DevNews.cs
+++ HttpClient/DevNews.cs
@@ -18,19 +18,40 @@ public  class Client
         public int userId { get; set; }
         public int id { get; set; }
         public string title { get; set; }
-        public string bady { get; set; }
+        public string body { get; set; }
     }
     public async Task fetchPosts(int numPosts)
     {
 
         var response = await client.GetAsync(this.url);
+        //Console.WriteLine("response: " + response);
         string answer = await response.Content.ReadAsStringAsync();
+        //Console.WriteLine("answer: " + answer);
         Post[] posts = JsonSerializer.Deserialize<Post[]>(answer);
 
-        for (int i = 0; i < numPosts; i++)
+        for (int i = 0; i < numPosts && i < posts.Length; i++)
         {
             Post post = posts[i];
             Console.WriteLine(post.title);
+        }
+    }
+
+    public async Task postById(int id)
+    {
+        string newUrl = this.url + "?id=" + id;
+        var response = await client.GetAsync(newUrl);
+        string text = await response.Content.ReadAsStringAsync();
+        Post[] posts = JsonSerializer.Deserialize<Post[]>(text);
+        if (posts.Length > 0)
+        {
+            foreach (Post post in posts)
+            {
+                Console.WriteLine($"Post ID: {post.id}\nTitle: {post.title}\nbady: {post.body}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("The post number does not exist.");
         }
     }
 
